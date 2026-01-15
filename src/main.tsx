1902dc090a6a320ce00a94b8b5a8c3582b87975c
Isabel Sáenz <isasaete@gmail.com>
2026-01-15 20:51:23 +0100

    Add Hero, Layout & App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./UI/components/App/App";
+import "@fontsource-variable/lexend/index.css";
+import "@fontsource-variable/lexend-exa/index.css";
+import "@fontsource-variable/lexend-peta/index.css";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="main-container"></div>
-  </StrictMode>,
+    <App />
+  </StrictMode>
 );
