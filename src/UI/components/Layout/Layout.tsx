7f0ff2bb5e60ac9d55665140dde5df32f165cca6
Isabel Sáenz <isasaete@gmail.com>
2026-02-27 15:11:20 +0100

    Add Navbar & Contact with responsive Layout

--- src/UI/components/Layout/Layout.tsx
+++ src/UI/components/Layout/Layout.tsx
@@ -1,14 +1,25 @@
+import Contact from "../../../portfolio/components/Contact/Contact";
 import Hero from "../../../portfolio/components/Hero/Hero";
 import { isabelSaenzLinks } from "../../../portfolio/data/links";
 import { isaDataProfile } from "../../../portfolio/data/profile";
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
 import "./Layout.css";
 
 const Layout: React.FC = () => {
   return (
     <>
-      <main className="layout">
-        <Hero profileData={isaDataProfile} links={isabelSaenzLinks} />
-      </main>
+      <NavBar />
+      <div className="main-container">
+        <main className="layout">
+          <Hero profileData={isaDataProfile} />
+          <Contact links={isabelSaenzLinks} />
+        </main>
+        <Footer
+          developerName={isaDataProfile.name}
+          location={isaDataProfile.location}
+        />
+      </div>
     </>
   );
 };
